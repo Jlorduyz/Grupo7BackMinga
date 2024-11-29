06d24b2ce65f9b8b9795074b19b87f98ae3d953d
MauricioYPS <yepes060@gmail.com>
2024-11-29 18:42:08 -0500

    adelanto user/data CRUD

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import 'dotenv/config';
 import './config/database.js';
-
+import cors from "cors";
+import morgan from "morgan";
+import indexRouter from "./router/index.js";
 const server = express();
 const PORT = process.env.PORT || 8080;
 
 
-server.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+const ready = () => {console.log(`Server running on port ${PORT}`);}
 
-server.listen(PORT,()=>{
-    console.log(`Server listening on port ${PORT}`);
-})
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(cors());
+server.use(morgan("dev"));
+
+server.use('/api', indexRouter);
+
+server.listen(PORT, ready);
