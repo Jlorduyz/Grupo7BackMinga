16f6c9c8c8f75591f89b3cecd04b81d1ac851adc
MauricioYPS <yepes060@gmail.com>
2024-12-03 20:12:45 -0500

    mas endpoints para consumo

--- router/chapters.js
+++ router/chapters.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { read,readByID } from "../controllers/chapters/read.js";
+import { read,readByID, readByMangaID, readPagesByID } from "../controllers/chapters/read.js";
 import { create } from "../controllers/chapters/create.js";
 import { updateChapter } from "../controllers/chapters/update.js";
 import { remove } from "../controllers/chapters/delete.js";
@@ -8,6 +8,8 @@ const router = Router();
 
 router.get('/all',passport.authenticate("jwt", {session: false}),read)
 router.get('/id/:id',passport.authenticate("jwt", {session: false}),readByID)
+router.get('/manga/id/:id',passport.authenticate("jwt", {session: false}),readByMangaID)
+router.get('/pages/id/:id',passport.authenticate("jwt", {session: false}),readPagesByID)
 router.post('/create',passport.authenticate("jwt", {session: false}),create)
 router.put('/update/:id',passport.authenticate("jwt", {session: false}),updateChapter)
 router.delete('/delete/:id',passport.authenticate("jwt", {session: false}),remove)
