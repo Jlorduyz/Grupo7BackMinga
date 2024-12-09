634bbe85b86c672650edf4c3209e3703b35ccdcb
MauricioYPS <yepes060@gmail.com>
2024-12-09 04:52:13 -0500

    modificaciones necesarias para el admin-panel

--- router/index.js
+++ router/index.js
@@ -6,11 +6,13 @@ import ChapterRouter from "./chapters.js";
 import CategoriesRouter from "./categories.js";
 import routerAuth from "./auth.js";
 import ReactionRouter from "./reactions.js";
+import AutorRouter from "./authors.js";
 
 const router = Router();
 
 router.use('/users',userRouter)
 router.use('/companies',companyRouter)
+router.use('/authors',AutorRouter)
 router.use('/mangas',mangaRouter)
 router.use('/chapters',ChapterRouter)
 router.use('/categories',CategoriesRouter)
