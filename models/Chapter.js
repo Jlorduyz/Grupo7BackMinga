098537f0611062ef27285af175f1e6fbeb4790e0
MauricioYPS <yepes060@gmail.com>
2024-12-09 20:48:56 -0500

    Mergeo

--- models/Chapter.js
+++ models/Chapter.js
@@ -4,7 +4,7 @@ let collection = "chapters"
 let schema = new Schema({
     manga_id:{type: Schema.Types.ObjectId, ref: "mangas", required: true},
     title:{type:String,required:true},
-    cover_photo:{type:String,required:true},
+    cover_photo:{type:String,required:false},
     pages:{type:[String],required:true},
     order:{type:Number,required:true},
 },{
--- router/users.js
+++ router/users.js
@@ -14,7 +14,7 @@ router.get('/all',passport.authenticate("jwt", {session: false}),allUsers)
 router.get('/validationToken',passport.authenticate("jwt", {session: false}),oneUser)
 router.get('/id/:id',passport.authenticate("jwt", {session: false}),userByID)
 router.post('/register',accountExist,createHash,register )
-router.put('/update/:id',passport.authenticate("jwt", {session: false}),updateUser)
+router.put('/update/:id',passport.authenticate("jwt", {session: false}),createHash,updateUser)
 router.put('/update/email/:email',passport.authenticate("jwt", {session: false}),updateUserByEmail)
 router.delete('/delete/:id',passport.authenticate("jwt", {session: false}),deleteUser)
 
